b835b7c30b2dd5f6bc14437ca0b8ffde02ef6b23
Divya <divyakumarisingh546@gmail.com>
2023-07-25 16:52:22 +0530

    First commit

---
+++ Controllers/GreetingsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace greetingsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GreetingsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/greetings")]
+        public IActionResult Greet()
+        {
+            //will return HTTP status code and Message/data
+            return Ok("Hello and Welcome to WebAPI developers world");
+        }
+        [HttpGet]
+        [Route("/greetings/{guestName}")]
+        public IActionResult Greet(string guestName)
+        {
+            if(guestName == "")
+            {
+                return NoContent();
+            }
+            else
+            {
+                return Ok("Hello" + guestName);
+            }
+        }
+
+        [HttpGet]
+        [Route("/List/techlist")]
+        public IActionResult TechnologiesList()
+        {
+            string[] techlist = new string[5];
+            techlist[0] = ".Net";
+            techlist[1] = "Java";
+            techlist[2] = "Angular";
+            techlist[3] = "React";
+            techlist[4] = "Azure";
+            return Ok(techlist);
+        }
+    }
+}
